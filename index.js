a09803ac27d17d94d75c4c55f581c8cb47dd562f
khoavt02 <khoavt02@gmail.com>
2022-04-06 20:43:30 +0700

    Add new project The band

---
+++ index.js
@@ -0,0 +1,14 @@
+/*const buyTickets = document.querySelectorAll('.js-buy')
+const modal = document.querySelector('.js-modal')
+const modalClose = document.querySelector('.js-close')
+function showBuyTickets() {
+    modal.classList.add('open')
+}
+function hideBuyTickets() {
+    modal.classList.remove('open')
+}
+for (const buyTicket of buyTickets){
+    buyTicket.addEventListener('click', showBuyTickets)
+}
+modalClose.addEventListener('click', hideBuyTickets)
+*/
